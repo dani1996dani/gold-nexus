ddaf0c66bc4dbb51a8eb9c77a627fd44d551104f
Daniel Baron <dani1996dani@gmail.com>
2025-12-08 19:29:01 -0500

    fix build error by adding loading state for marketplace

---
+++ src/app/marketplace/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-[50vh] w-full items-center justify-center">
+      <p className="text-muted-foreground">Loading Market...</p>
+    </div>
+  );
+}
