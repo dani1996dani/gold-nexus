83caab585df9788a7966185fc0b0ba4d8561b02c
Daniel Baron <dani1996dani@gmail.com>
2025-12-20 19:49:13 -0500

    fix error

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -89,7 +89,7 @@ export async function PUT(request: Request) {
     return NextResponse.json(updatedUser);
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return NextResponse.json({ error: error.errors[0].message }, { status: 400 });
+      return NextResponse.json({ error: error.issues[0].message }, { status: 400 });
     }
     console.error('Update User Error:', error);
     return NextResponse.json({ error: 'An unexpected error occurred' }, { status: 500 });
